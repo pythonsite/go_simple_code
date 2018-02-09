cf0081f2748eccd80516c294189a7c7442f5fd9d
zhaofan <zhaofan@commchina.net>
2018-02-09 14:56:40 +0800

    翻转字符串方法

---
+++ reverse/reverse.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func reverse(runes []rune) []rune{
+	// 将切片内容进行翻转
+	for i,j:=0,len(runes)-1;i<j;i,j = i+1,j-1{
+		runes[i],runes[j] = runes[j],runes[i]
+	}
+	return runes
+}
+
+func convertToSlice(str string) []rune{
+	// 将字符串转换为slice
+	res := []rune(str)
+	return res
+}
+
+func main() {
+	str := "hello,世界"
+	res := convertToSlice(str)
+	fmt.Println(string(res))
+	res2 := reverse(res)
+	fmt.Println(string(res2))
+}
