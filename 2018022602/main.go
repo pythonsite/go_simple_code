6d3f6da07b369da034b44e2dcd576ed5c5c4c79c
zhaofan <hjzhaofan@163.com>
2018-02-27 00:19:50 +0800

    日常练习

---
+++ 2018022602/main.go
@@ -0,0 +1,51 @@
+/*
+回文数字，由2位数字乘机获取的最大回文是9009=91*99
+ */
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// 将一个数字字符串进行反转
+func reverseNum(num string)string{
+	numSlice := []byte(num)
+	for i,j:=0,len(numSlice)-1;i<j;i,j = i+1,j-1{
+		numSlice[i],numSlice[j] = numSlice[j],numSlice[i]
+	}
+	res := string(numSlice)
+	return res
+
+}
+
+// 判断反转后的字符串是否和原字符串相等
+func judege(num string)(b bool){
+	res := reverseNum(num)
+	if num == res{
+		return true
+	}
+	return false
+}
+
+func main() {
+	var bigNum int
+	var one int
+	var two int
+	for i:=100;i<=999;i++{
+		for j:=100;j<=999;j++{
+			num := i*j
+			res := judege(strconv.Itoa(num))
+			if res == true{
+				if num > bigNum{
+					bigNum = num
+					one = i
+					two = j
+				}
+			}
+		}
+	}
+	fmt.Printf("%d*%d=%d",one,two,bigNum)
+
+}
